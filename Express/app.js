bf22fef09bff433289f1607d4c1865b40106b651
aamjit <aayanglem@gmail.com>
2021-12-02 23:54:32 +0530

    initial commit

---
+++ Express/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mdb = require('mongoose')
+
+app.use(express.json());
+// routes
+// get - [retreive information]
+// post - [send information]
+// delete - delete smthing
+// patch - update
+
+mdb.connect('mongodb+srv://amarjit:cdt10070@cluster0.hwj9j.mongodb.net/mdb-1?retryWrites=true&w=majority', (e)=>{
+    if(e) console.log(e.message)
+
+    else console.log('Database Connected')
+})
+
+app.get('/home', (req, res)=>{
+    // console.log('Welcome to Express JS')
+    res.send()
+    // res.json({body:{
+    //     message: 'Welcome to Express JS'
+    // }})
+})
+
+app.listen('3000', ()=>{
+    console.log("Server is up and Running!!")
+})
